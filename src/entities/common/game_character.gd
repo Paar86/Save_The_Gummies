0ae3a0172d9c474da6652162186019b87c8234c4
Paar86 <josu@seznam.cz>
2023-10-27 23:08:11 +0200

    Added support for movement modificators for player/goblin.

--- src/entities/common/game_character.gd
+++ src/entities/common/game_character.gd
@@ -4,6 +4,13 @@ signal effect_added(effect: Enums.effect)
 signal effect_removed(effect: Enums.effect)
 
 var _applied_effects: Array[Enums.effect] = []
+var _applied_movement_modificators: Array[Vector2] = []
+var _combined_movement_modificator: Vector2 = Vector2.ZERO
+
+# Returns a copy of the combined value
+var combined_movement_modificator: Vector2:
+	get:
+		return _combined_movement_modificator
 
 
 func apply_effect(effect: Enums.effect) -> void:
@@ -18,6 +25,21 @@ func remove_effect(effect: Enums.effect) -> void:
 
 	if not _applied_effects.has(effect):
 		effect_removed.emit(effect)
+
+
+func apply_movement_modificator(modificator: Vector2) -> void:
+	if not _applied_movement_modificators.has(modificator):
+		_combined_movement_modificator += modificator
+
+	_applied_movement_modificators.push_back(modificator)
+
+
+func remove_movement_modificator(modificator: Vector2) -> void:
+	_applied_movement_modificators.erase(modificator)
+
+	var occurrences = _applied_movement_modificators.count(modificator)
+	if occurrences == 0:
+		_combined_movement_modificator -= modificator
 
 
 func is_effect_active(effect: Enums.effect) -> bool:
