1b362d74ccbc5a5866c84dda6180909c1de5cf1d
Paar86 <josu@seznam.cz>
2023-10-30 22:13:50 +0100

    Minor formatting polishing in ball_creature.gd

--- src/entities/ball_creature/ball_creature.gd
+++ src/entities/ball_creature/ball_creature.gd
@@ -6,7 +6,7 @@ signal effect_removed(effect: Enums.effect)
 
 # Secondary velocity has different effect on RigidBody2D so we should be able to tweak it
 const VELOCITY_SECONDARY_SCALE: = 2.0
-enum colors {BLUE_DARK_BLUE, GREEN_DARK_GREEN, YELLOW_DARK_YELLOW, 
+enum colors {BLUE_DARK_BLUE, GREEN_DARK_GREEN, YELLOW_DARK_YELLOW,
 	MAGENTA_MAROON, PINK_MAROON, DARK_YELLOW_BROWN, WHEAT_YELLOW_BROWN}
 
 @export var color = colors.BLUE_DARK_BLUE : set = _set_color;
@@ -25,18 +25,22 @@ var _damp_default: float = ProjectSettings.get_setting("physics/2d/default_linea
 
 
 func _ready() -> void:
-	if not Engine.is_editor_hint():
-		_effects_applier_component.effect_added.connect(func(effect: Enums.effect) -> void: effect_added.emit(effect))
-		_effects_applier_component.effect_removed.connect(func(effect: Enums.effect) -> void: effect_removed.emit(effect))
+	if Engine.is_editor_hint():
+		return
 
-		effect_added.connect(_on_effect_added)
-		effect_removed.connect(_on_effect_removed)
+	_effects_applier_component.effect_added.connect(func(effect: Enums.effect) -> void: effect_added.emit(effect))
+	_effects_applier_component.effect_removed.connect(func(effect: Enums.effect) -> void: effect_removed.emit(effect))
+
+	effect_added.connect(_on_effect_added)
+	effect_removed.connect(_on_effect_removed)
 
 
 func _physics_process(delta: float) -> void:
-	if not Engine.is_editor_hint():
-		if _effects_applier_component.velocity_secondary != Vector2.ZERO:
-			apply_central_force(_effects_applier_component.velocity_secondary * VELOCITY_SECONDARY_SCALE)
+	if Engine.is_editor_hint():
+		return
+
+	if _effects_applier_component.velocity_secondary != Vector2.ZERO:
+		apply_central_force(_effects_applier_component.velocity_secondary * VELOCITY_SECONDARY_SCALE)
 
 
 func apply_effect(effect: Enums.effect) -> void:
