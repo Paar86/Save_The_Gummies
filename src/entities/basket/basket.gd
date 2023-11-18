1c87b50c06bc228a54670672e14757ce4dd9c9c7
AlexHCC <alexander06088@yahoo.com>
2023-11-18 12:16:13 +0200

    Adds basket level transition functionality

--- src/entities/basket/basket.gd
+++ src/entities/basket/basket.gd
@@ -1,8 +1,14 @@
 extends StaticBody2D
 
+const WAIT_TIME: = 2.0
+
 @onready var _animation_player: AnimationPlayer = $AnimationPlayer
+@onready var _creature_detector: Area2D = $CreatureDetector
 
 
 func on_creature_detector_body_entered(body: Node2D) -> void:
-	_animation_player.play("CREATURE_IN")
-	print("Ball creature entered the basket!")
+	if body is BallCreature:
+		_animation_player.play("CREATURE_IN")
+		await get_tree().create_timer(WAIT_TIME).timeout
+		if _creature_detector.overlaps_body(body):
+			Events.change_level.emit()
