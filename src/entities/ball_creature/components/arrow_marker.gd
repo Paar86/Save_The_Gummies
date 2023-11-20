fc2a9128f7f66c7efe03013209c39148826f8353
AlexHCC <alexander06088@yahoo.com>
2023-11-20 20:10:49 +0200

    Adds arrow marker above ball creature

---
+++ src/entities/ball_creature/components/arrow_marker.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+const AMPLITUDE: = 4.0
+const FREQUENCY: = 0.01
+const INITIAL_POSITION: = 12.0
+
+
+func _physics_process(delta: float) -> void:
+	if not self.visible:
+		return
+	
+	position.y = AMPLITUDE*sin(Time.get_ticks_msec()*FREQUENCY) - INITIAL_POSITION
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		self.visible = true
+	if event.is_action_released("ui_accept"):
+		self.visible = false
