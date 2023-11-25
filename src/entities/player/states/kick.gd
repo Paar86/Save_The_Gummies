69c9ae31d316785c3bec69813967410185b43696
Paar86 <josu@seznam.cz>
2023-11-25 23:03:23 +0100

    Fixed wrong animation handling in Whistle and Kick states.

--- src/entities/player/states/kick.gd
+++ src/entities/player/states/kick.gd
@@ -4,16 +4,15 @@ extends State
 var _kick_sfx: Resource = preload(SfxResources.PLAYER_KICK)
 
 
-func _ready() -> void:
-	# We need to wait until the player node is ready so it has all onready properties initialized
-	await owner.ready
-	character.animation_player.animation_finished.connect(_on_kick_timeout)
-
-
 func on_enter(params: StateParams) -> void:
+	character.animation_player.animation_finished.connect(_on_kick_timeout)
 	AudioStreamManager2D.play_sound(_kick_sfx, character)
 	state_machine.change_animation("kick")
 	character.animation_player.play("small_jump")
+
+
+func on_exit() -> void:
+	character.animation_player.animation_finished.disconnect(_on_kick_timeout)
 
 
 func _on_kick_timeout(animation_name: String) -> void:
--- src/entities/player/states/whistle.gd
+++ src/entities/player/states/whistle.gd
@@ -4,17 +4,16 @@ extends State
 var _whistle_sfx: Resource = preload(SfxResources.PLAYER_WHISTLE)
 
 
-func _ready() -> void:
-	# We need to wait until the player node is ready so it has all onready properties initialized
-	await owner.ready
-	character.animation_player.animation_finished.connect(_on_animation_timeout)
-
-
 func on_enter(params: StateParams) -> void:
+	character.animation_player.animation_finished.connect(_on_animation_timeout)
 	AudioStreamManager2D.play_sound(_whistle_sfx, character)
 	state_machine.change_animation("whistle")
 	character.animation_player.play("small_jump")
 
 
-func _on_animation_timeout() -> void:
+func on_exit() -> void:
+	character.animation_player.animation_finished.disconnect(_on_animation_timeout)
+
+
+func _on_animation_timeout(animation_name: String) -> void:
 	state_machine.transition_to("Idle")
