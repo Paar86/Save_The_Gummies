02de4eca8514ffe39bae4c29bc799320f9003c85
AlexHCC <alexander06088@yahoo.com>
2023-12-07 23:22:04 +0200

    Pause only during play

--- src/user_interface/pause_screen/pause_screen.gd
+++ src/user_interface/pause_screen/pause_screen.gd
@@ -1,10 +1,25 @@
 extends CanvasLayer
 
+var _pause = true
+
+
+func _ready() -> void:
+	Events.reload_level.connect(_on_reload)
+	Events.player_dead.connect(_on_player_dead)
+
 
 func _on_continue_button_up() -> void:
 	Events.pause_level.emit()
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
+	if _pause and event.is_action_pressed("pause"):
 		Events.pause_level.emit()
+
+
+func _on_reload() -> void:
+	_pause = true
+
+
+func _on_player_dead() -> void:
+	_pause = false
