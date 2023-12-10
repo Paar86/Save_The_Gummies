d6c312584483abf9b3fed05212a2e86d816a0ab2
Paar86 <josu@seznam.cz>
2023-12-10 11:49:24 +0100

    More animations for ball creature.

--- src/entities/ball_creature/states/carried.gd
+++ src/entities/ball_creature/states/carried.gd
@@ -4,7 +4,19 @@ extends State
 
 
 func on_enter(params: StateParams) -> void:
-	pass
+#	var animation_name = "looking_around"
+#	var animation = ball_creature.animation_player.get_animation(animation_name)
+#	animation.loop_mode = Animation.LOOP_LINEAR
+
+	ball_creature.animation_player.play("looking_around_stressed")
+
+
+func on_exit() -> void:
+#	var animation_name = "looking_around"
+#	var animation = ball_creature.animation_player.get_animation(animation_name)
+#	animation.loop_mode = Animation.LOOP_NONE
+
+	ball_creature.animation_player.play("RESET")
 
 
 func physics_process(delta: float) -> void:
--- src/entities/ball_creature/states/idle.gd
+++ src/entities/ball_creature/states/idle.gd
@@ -1,15 +1,19 @@
 extends State
 
-const BLINKING_DEFAULT_TIME: = 3.0
-
 @export var ball_creature: BallCreature
+
+var _idle_animations_weights = {
+	"blinking": 0.75,
+	"looking_around": 0.25,
+}
+
 @onready var _animation_delay_timer: Timer = $AnimationDelayTimer
-@onready var _blinking_delay_timer: Timer = $BlinkingDelayTimer
+@onready var _next_animation_delay_timer: Timer = $NextAnimationDelayTimer
 
 
 func _ready() -> void:
 	_animation_delay_timer.connect("timeout", _on_animation_delay_timeout)
-	_blinking_delay_timer.connect("timeout", _on_blinking_delay_timeout)
+	_next_animation_delay_timer.connect("timeout", _on_next_animation_delay_timeout)
 
 
 func on_enter(params: StateParams) -> void:
@@ -19,8 +23,7 @@ func on_enter(params: StateParams) -> void:
 
 func on_exit() -> void:
 	_animation_delay_timer.stop()
-	_blinking_delay_timer.stop()
-	_blinking_delay_timer.wait_time = BLINKING_DEFAULT_TIME
+	_next_animation_delay_timer.stop()
 	ball_creature.animation_player.disconnect("animation_finished", _on_animation_finished)
 	ball_creature.animation_player.play("RESET")
 
@@ -37,14 +40,20 @@ func physics_process(delta: float) -> void:
 
 func _on_animation_delay_timeout() -> void:
 	ball_creature.animation_player.play("idle")
-	_blinking_delay_timer.start()
+	_next_animation_delay_timer.start()
 
 
-func _on_blinking_delay_timeout() -> void:
-	ball_creature.animation_player.play("blinking")
+func _on_next_animation_delay_timeout() -> void:
+	var weight = randf()
+	var next_animation_name = "blinking"
+	for animation in _idle_animations_weights:
+		if weight >= _idle_animations_weights[animation]:
+			next_animation_name = animation
+			break
+
+	ball_creature.animation_player.play(next_animation_name)
 
 
 func _on_animation_finished(animation_name: StringName) -> void:
-	if animation_name == "blinking":
-		_blinking_delay_timer.wait_time = randf_range(0.15, 3.0)
-		_blinking_delay_timer.start()
+	_next_animation_delay_timer.wait_time = randf_range(0.15, 3.0)
+	_next_animation_delay_timer.start()
