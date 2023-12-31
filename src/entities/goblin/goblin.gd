ac93b6387a73c20e407fb08290dfe05ebf24b37a
Paar86 <josu@seznam.cz>
2023-12-31 19:15:40 +0100

    Better obstacle detection for Goblin enemy.

--- src/entities/goblin/goblin.gd
+++ src/entities/goblin/goblin.gd
@@ -4,7 +4,6 @@ class_name Goblin extends GameCharacter
 signal whistle_heard(direction: float)
 
 ## Flips the horizontal orientation of the enemy in editor.
-## Don't forget to enable children in editor for the change to make desired effect in-game!
 @export var change_direction_in_editor: bool:
 	set(value):
 		var animated_sprite = get_node("AnimatedSprite2D")
@@ -12,6 +11,7 @@ signal whistle_heard(direction: float)
 		var ground_det = get_node("GroundDetector")
 		var facing_direction = sign(animated_sprite.scale.x) * -1
 		player_detector.scale.x = sign(facing_direction)
+		obstacle_detector.scale.x = sign(facing_direction)
 		animated_sprite.scale.x = sign(facing_direction)
 		hitbox_component.position = Vector2(abs(hitbox_component.position.x) * facing_direction, hitbox_component.position.y)
 		ground_det.position = Vector2(abs(ground_det.position.x) * facing_direction, ground_det.position.y)
@@ -23,6 +23,7 @@ var _facing_direction: = 1.0
 @onready var player_detector: RayCast2D = $PlayerDetectorRayCast
 @onready var state_machine: StateMachine = $StateMachine
 @onready var ground_detector: RayCast2D = $GroundDetector
+@onready var obstacle_detector: RayCast2D = $ObstacleDetector
 
 @onready var _reaction_symbol: ReactionSymbol = $ReactionSymbol
 @onready var _animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -49,11 +50,14 @@ func toggle_hitbox_collider(value: bool) -> void:
 
 
 func change_direction(new_direction: float) -> void:
-	_facing_direction = new_direction
-	player_detector.scale.x = sign(new_direction)
-	_animated_sprite.scale.x = sign(new_direction)
-	_hitbox_component.set_deferred("position", Vector2(abs(_hitbox_component.position.x) * new_direction, _hitbox_component.position.y))
-	ground_detector.set_deferred("position", Vector2(abs(ground_detector.position.x) * new_direction, ground_detector.position.y))
+	(func():
+		_facing_direction = new_direction
+		player_detector.scale.x = sign(new_direction)
+		obstacle_detector.scale.x = sign(new_direction)
+		_animated_sprite.scale.x = sign(new_direction)
+		_hitbox_component.position = Vector2(abs(_hitbox_component.position.x) * new_direction, _hitbox_component.position.y)
+		ground_detector.position = Vector2(abs(ground_detector.position.x) * new_direction, ground_detector.position.y)
+	).call_deferred()
 
 
 func propagate_whistle(source_body: GameCharacter) -> void:
