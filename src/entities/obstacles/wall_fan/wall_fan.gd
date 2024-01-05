1917566d7ad607c4217040a67bf5005a3fcff417
Paar86 <josu@seznam.cz>
2024-01-05 15:16:29 +0100

    Added unpassable wall fan.

--- src/entities/obstacles/wall_fan/wall_fan.gd
+++ src/entities/obstacles/wall_fan/wall_fan.gd
@@ -1,17 +1,18 @@
+@tool
 class_name WallFan extends Area2D
 
-const WIND_STRENGTH: float = 35.0
+@export var wind_strength: = 35.0
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is GameCharacter or body is BallCreature:
-		var wind_velocity := Vector2(transform.x.x * WIND_STRENGTH, 0.0)
+		var wind_velocity := Vector2(transform.x.x * wind_strength, 0.0)
 		body.apply_movement_modificator(wind_velocity)
 		body.apply_effect(Enums.effect.WIND)
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is GameCharacter or body is BallCreature:
-		var wind_velocity := Vector2(transform.x.x * WIND_STRENGTH, 0.0)
+		var wind_velocity := Vector2(transform.x.x * wind_strength, 0.0)
 		body.remove_movement_modificator(wind_velocity)
 		body.remove_effect(Enums.effect.WIND)
--- src/entities/obstacles/wind_generator/wind_particles.gd
+++
@@ -1,5 +0,0 @@
-extends GPUParticles2D
-
-
-func _ready() -> void:
-	(process_material as ParticleProcessMaterial).gravity.x *= transform.x.x
