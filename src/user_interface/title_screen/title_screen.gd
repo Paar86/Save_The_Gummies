bede1921e6d4f7654a88260ef2c980932efb02a6
Paar86 <josu@seznam.cz>
2024-01-20 21:18:33 +0100

    Title screen logo animation + start prompt.

--- src/user_interface/title_screen/title_screen.gd
+++ src/user_interface/title_screen/title_screen.gd
@@ -2,18 +2,25 @@ class_name TitleScreen extends Node
 
 const BALL_CRETURE_VISIBILITY_THRESHOLD: = 32
 const BALL_CREATURE_CONSTANT_FORCE: = 300.0
+const LOGO_SHAKE_OFFSET: = 3.0
+const LOGO_SHAKE_COUNT: = 5
+const LOGO_SHAKE_FREQ: = 0.025
+const START_LABEL_BLINK_FREQ: = 1.0
 
+var _start_requested: = false
 var _initial_level_number: = 1
 var _ball_creatures_all: Array[BallCreature] = []
-@onready var _menu_options: = $MenuOptions as MainMenuOptions
+
+@onready var _logo: = $Logo as Sprite2D
+@onready var _start_game_label: = $StartGameLabel as Label
 
 
 func _ready() -> void:
 	_ball_creatures_all.assign(find_children("*", "BallCreature"))
-	#for ball_creature in _ball_creatures_all:
-		#ball_creature.sleeping = true
+	for ball_creature in _ball_creatures_all:
+		ball_creature.freeze = true
 
-	#set_process_input(false)
+	set_process_input(false)
 	set_physics_process(false)
 
 
@@ -29,6 +36,22 @@ func _physics_process(delta: float) -> void:
 
 	if _are_all_creatures_freed():
 		Events.change_scene_requested.emit()
+
+
+func shake_logo() -> void:
+	for ball_creature in _ball_creatures_all:
+		ball_creature.freeze = false
+
+	_show_start_game_prompt()
+
+	var original_logo_position: = _logo.position
+	for i in LOGO_SHAKE_COUNT:
+		_logo.position.y = original_logo_position.y + LOGO_SHAKE_OFFSET
+		await get_tree().create_timer(LOGO_SHAKE_FREQ).timeout
+		_logo.position.y = original_logo_position.y - LOGO_SHAKE_OFFSET
+		await get_tree().create_timer(LOGO_SHAKE_FREQ).timeout
+
+	_logo.position = original_logo_position
 
 
 func _check_ball_creature_visibility(index: int) -> void:
@@ -53,6 +76,8 @@ func _are_all_creatures_freed() -> bool:
 
 
 func _on_game_start() -> void:
+	_start_requested = true
+	_start_game_label.hide()
 	set_physics_process(true)
 
 	var viewport_half: float = get_viewport().get_visible_rect().size.x / 2
@@ -63,8 +88,11 @@ func _on_game_start() -> void:
 
 
 func _show_start_game_prompt() -> void:
-	#TODO: Show label
 	set_process_input(true)
+
+	while not _start_requested:
+		_start_game_label.visible = !_start_game_label.visible
+		await get_tree().create_timer(START_LABEL_BLINK_FREQ).timeout
 
 
 func _on_every_ball_creature_not_visible() -> void:
